2361510ca12e49d53224957cf9850d37904b1a20
Millah06 <abdullahialiyugarba14@gmail.com>
2026-03-05 03:21:24 +0000

    package.json edited, from build: tsc to build: risma generate && tsc

--- src/prisma.ts
+++ src/prisma.ts
@@ -16,6 +16,11 @@ import { PrismaPg } from "@prisma/adapter-pg";
 
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
-export const prisma = globalForPrisma.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma = global.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
