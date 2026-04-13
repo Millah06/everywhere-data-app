859c1c616705a026205259f795b5004aa105c76b
Millah06 <abdullahialiyugarba14@gmail.com>
2026-04-13 16:01:52 +0100

    forgot to add route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,6 +7,7 @@ import kycRouter from "../modules/kyc/routes/routes";
 import userRouter from "../modules/user/routes/routes";
 import authRouter from "../modules/auth/routes/routes";
 import walletRouter from "../modules/wallet/routes/routes";
+import supportChatRouter from "../modules/support/routes/routes";
 
 const router = Router();
 
@@ -18,6 +19,7 @@ router.use(kycRouter);
 router.use(userRouter);
 router.use(authRouter);
 router.use(walletRouter);
+router.use(supportChatRouter);
 
 
 
