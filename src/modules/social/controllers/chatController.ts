37c6642c56dc1e9978748525c8cc8507c4d6da8c
Millah06 <abdullahialiyugarba14@gmail.com>
2026-04-17 17:05:48 +0100

    oh my god

--- src/modules/social/controllers/chatController.ts
+++ src/modules/social/controllers/chatController.ts
@@ -3,7 +3,6 @@ import admin from 'firebase-admin';
 const createdOrGetChat = async (req: any, res: any) => {
   try {
     const { userId } = req.body;
-    const { postId } = req.body;
   }
   catch (error) {    console.error('Error creating or getting chat:', error);
     res.status(500).json({ error: 'Internal Server Error' });
